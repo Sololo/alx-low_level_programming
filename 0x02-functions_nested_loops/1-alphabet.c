2b9c49b45b5729e4756767193fed2560c079a7a6
Sololo <philasololo@gmail.com>
2022-09-13 16:23:30 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
