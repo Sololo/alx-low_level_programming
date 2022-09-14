c91b9c23cc31caab5b8abbbbebf4d614d1a698c2
Sololo <philasololo@gmail.com>
2022-09-14 04:00:33 -0700

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints natural numbers below 1024 and are multiples of 3
+ *
+ * Return: 0 (Always).
+ */
+int main(void)
+{
+	int x. y;
+	
+	for (x = 1; x < 1024; x++)
+	{
+		if ((x % 3) == || (x % 5) ==0)
+			y += x;
+	}
+	printf("%d\n", y);
+	return (0);
+}
