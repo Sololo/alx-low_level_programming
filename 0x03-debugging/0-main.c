a871f8145cfce2fec5aec55558949c4ae940429d
Sololo <philasololo@gmail.com>
2022-09-15 13:07:05 -0700

    Added 0-main.c main.h files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - test function that prints if interger is + or -
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
