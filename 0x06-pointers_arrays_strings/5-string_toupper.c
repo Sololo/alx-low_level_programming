4b6c03352c6b7c55491e631796c7207d795f5ef5
Sololo <philasololo@gmail.com>
2022-09-22 09:02:55 -0700

    Added 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase.
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *s)
+{
+	int x = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] >= 'a' && s[x] <= 'z')
+			s[x] = s[x] - 32;
+	}
+
+	return (s)
+}
