555bcd8005b90b135abdf4603e8ed4efb62b52d8
Sololo <philasololo@gmail.com>
2022-10-01 10:22:34 -0700

    Added 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all recieved arguments
+ * @argc: numberof arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	x = 0;
+	y = argc - 1;
+
+	while (y >= 0)
+	{
+		printf("%s\n", argv[x]);
+		x++;
+		y--;
+	}
+}
