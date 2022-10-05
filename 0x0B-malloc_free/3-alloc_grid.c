90a711d96b0853eb42318edade20eaf536ab7082
Sololo <philasololo@gmail.com>
2022-10-05 05:32:58 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i < height; i ++)
+	for (i = 0; i < height; i++)
 	{
 		x[i] = malloc(width * sizeof(int));
 		if (x[i] == NULL)
