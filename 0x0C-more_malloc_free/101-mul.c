845c38649ed1c19dd3a0cdf9a05ecdff31cf0c02
Sololo <philasololo@gmail.com>
2022-10-06 22:06:24 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -58,24 +58,20 @@ void errors(void)
 
 int main (int argc, char argv[])
 {
-	char*s1, *s2;
-	int l1, l2, l, i, c, d1, d2, *result, x =0;
+	char *s1, *s2;
+	int l1, l2, l, i, c, d1, d2, *result, x = 0;
 
 	s1 = argc[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	l = l1 + l2 + 1;
 	result = malloc(sizeof(int) * l);
-
 	if (!result)
 		return (1);
-	
 	for (i = 0; i <= l1 + l2; i++)
 		result[i];
-		
 	for (l1 = l1 - 1; l1 >= 0; l1--)
 	{
 		d1 = s1[l2] - '0';
@@ -86,12 +82,10 @@ int main (int argc, char argv[])
 			c += result[l1 + l2 + 1] + (d1 * d2);
 			result[l1 + l2 + 1] = c % 10;
 			c /= 10;
-		}
-		
+		}	
 		if (c > 0)
 			result[l1 + l2 + 1] += c;
 	}
-	
 	for (i = 0; i < l - 1; i++)
 	{
 		if (result[i])
@@ -99,7 +93,6 @@ int main (int argc, char argv[])
 		if (a)
 			_putchar(result[i] + '0');
 	}
-	
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
