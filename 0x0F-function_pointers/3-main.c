c604d0275e8b05bd1744c3863107a651aaec0142
Sololo <philasololo@gmail.com>
2022-10-12 15:54:35 -0700

    Added 3-main.c 3-op_functions.c 3-get_op_func.c 3-calc.h files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	op = get_op_func(argv[2]);
+
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+
+	printf("%\n", op(a, b));
+	return (0);
+}
